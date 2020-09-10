b8e54198c5e689d3beaa1153eab58efbda4c582a
201700470022 <luan.goncalves@itec.ufpa.br>
2020-09-10 18:39:30 -0300

    First test

---
+++ client_sender.py
@@ -0,0 +1,83 @@
+from vision.ssd.vgg_ssd import create_vgg_ssd, create_vgg_ssd_predictor
+from vision.ssd.vgg_ssd_new import create_vgg_ssd_new
+from vision.ssd.predictor import PredictorM1, PredictorM2
+from vision.ssd.config import vgg_ssd_config as config
+import socket
+import sys
+import cv2
+import json
+
+print("# Preparing the server model.")
+
+model_path = "./models/ufpark-model.pth"
+split = 0   # Split point int the first maxpooling layer.
+label_path = "./models/ufpark-model-labels.txt"
+
+class_names = [name.strip() for name in open(label_path).readlines()]
+
+print("# Creating ssd model.")
+model = create_vgg_ssd(len(class_names), is_test=True)
+
+model.load(model_path)
+model1, _ = create_vgg_ssd_new(
+    len(class_names), model, split=split, is_test=True
+)
+
+predictor_m1 = PredictorM1(
+        model1,
+        config.image_size,
+        config.image_mean,
+        device=None,
+    )
+print("# Done!")
+
+print("# Preparing server socket.")
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Connect the socket to the port where the server is listening
+server_address = ('200.239.93.228', 10000)
+sock.connect(server_address)
+
+print("# Done!")
+
+print("# Start processing!")
+
+orig_image = cv2.imread('../samurai/dataset/JPEGImages/afternoon_1_scene_0015001.jpg')
+image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
+height, width, _ = image.shape
+output = predictor_m1.predict(image)
+
+output = output.cpu().numpy().tolist()
+
+filename = "data.json"
+
+with open(filename, "w") as outfile:
+    json.dump({"scores": output, "height":height, "width":width}, outfile)
+
+print("# Done!")
+
+print("# Sending scores.")
+try:
+    
+    # Send data
+    # message = 'This is the message.  It will be repeated.'
+    # print >>sys.stderr, 'sending "%s"' % message
+    # sock.sendall(message)
+
+    with open(filename, "rb") as f:
+        socket.sendall(bytes_read)
+
+    # # Look for the response
+    # amount_received = 0
+    # amount_expected = len(message)
+    
+    # while amount_received < amount_expected:
+    #     data = sock.recv(16)
+    #     amount_received += len(data)
+    #     print >>sys.stderr, 'received "%s"' % data
+
+finally:
+    print('Closing socket')
+    sock.close()
---
+++ server_receiver.py
@@ -0,0 +1,68 @@
+from vision.ssd.vgg_ssd import create_vgg_ssd, create_vgg_ssd_predictor
+from vision.ssd.vgg_ssd_new import create_vgg_ssd_new
+from vision.ssd.predictor import PredictorM1, PredictorM2
+from vision.ssd.config import vgg_ssd_config as config
+import socket
+import sys
+import json
+
+print("# Preparing the server model.")
+
+model_path = "./models/ufpark-model.pth"
+split = 0   # Split point int the first maxpooling layer.
+label_path = "./models/ufpark-model-labels.txt"
+
+class_names = [name.strip() for name in open(label_path).readlines()]
+
+print("# Creating ssd model.")
+model = create_vgg_ssd(len(class_names), is_test=True)
+
+model.load(model_path)
+_, model2 = create_vgg_ssd_new(
+    len(class_names), model, split=split, is_test=True
+)
+
+predictor_m2 = PredictorM2(
+        model2,
+        nms_method=None,
+        iou_threshold=config.iou_threshold,
+        candidate_size=200,
+        device=None,
+    )
+print("# Done!")
+
+print("# Preparing server socket.")
+
+# Create a TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the port
+server_address = ('0.0.0.0', 10000)
+sock.bind(server_address)
+
+print("# Done!")
+
+print("# Listen for incoming connections.")
+
+# Listen for incoming connections
+sock.listen(1)
+
+while True:
+    # Wait for a connection
+    connection, client_address = sock.accept()
+
+    try:
+        while True:
+            data = connection.recv(16)
+            if data:
+                with open(filename) as json_file:
+                    input_batch = torch.tensor(json.load(json_file)["scores"])
+                    height = torch.tensor(json.load(json_file)["height"])
+                    width = torch.tensor(json.load(json_file)["width"])
+                input_batch = input_batch.cuda()
+                boxes, labels, probs = predictor_m2.predict(input_batch, height, width, 30, 0.4)
+                connection.sendall(data)
+            
+    finally:
+        # Clean up the connection
+        connection.close()

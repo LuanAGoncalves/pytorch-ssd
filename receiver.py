85c08f7352488f25773db55055136927fcce5442
LuanAGoncalves <luan.goncalves@itec.ufpa.br>
2020-11-14 17:45:46 -0300

    test 2

--- receiver.py
+++ receiver.py
@@ -9,21 +9,47 @@ import json
 import torch
 import os
 import time
+import argparse
+import cv2
+from imutils import build_montages
+from datetime import datetime
+import numpy as np
+import imagezmq
+import imutils
+import torch
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-m", "--model-path", required=False,
+        help="path to the trained model", type=str, default="./models/ufpark-model.pth")
+    ap.add_argument("-l", "--label-path", required=False,
+        help="SSD labels", type=str, default="./models/ufpark-model-labels.txt")
+    ap.add_argument("-s", "--split", required=False,
+        help="split points on SSD: 0, 1, 2, 3 or 4", type=str, default=0)
+    ap.add_argument("-m", "--model-path", required=False,
+        help="path to the trained model", type=str, default="./models/ufpark-model.pth")
+    ap.add_argument("-l", "--label-path", required=False,
+        help="SSD labels", type=str, default="./models/ufpark-model-labels.txt")
+    ap.add_argument("-p", "--split-point", required=False,
+        help="split points on SSD: 0, 1, 2, 3 or 4", type=int, default=0)
+    args = vars(ap.parse_args())
+
+imageHub = imagezmq.ImageHub()
+
+lastActive = {}
+lastActiveCheck = datetime.now
 
 print("# Preparing the server model.")
 
-model_path = "./models/ufpark-model.pth"
-split = 0  # Split point int the first maxpooling layer.
-label_path = "./models/ufpark-model-labels.txt"
 times = []
 
-class_names = [name.strip() for name in open(label_path).readlines()]
+class_names = [name.strip() for name in open(args["label_path"]).readlines()]
 
 print("# Creating ssd model.")
 model = create_vgg_ssd(len(class_names), is_test=True)
 
-model.load(model_path)
-_, model2 = create_vgg_ssd_new(len(class_names), model, split=split, is_test=True)
+model.load(args["model_path"])
+_, model2 = create_vgg_ssd_new(len(class_names), model, split=args["split_point"], is_test=True)
 
 predictor_m2 = PredictorM2(
     model2,
@@ -34,9 +60,6 @@ predictor_m2 = PredictorM2(
 )
 print("# Done!")
 
-HOST = "0.0.0.0"
-PORT = 2345
-
 # If server and client run in same local directory,
 # need a separate place to store the uploads.
 try:
@@ -44,52 +67,70 @@ try:
 except FileExistsError:
     pass
 
-s = socket.socket()
-s.bind((HOST, PORT))
-s.listen(10)
-print("Waiting for a connection.....")
+# start looping over all the frames
+times = []
 
 while True:
-    conn, addr = s.accept()
-    print("Got a connection from ", addr)
-    connbuf = buffer.Buffer(conn)
+    (rpiName, jpg_buffer) = imageHub.recv_image()
+    image = cv2.imdecode(np.frombuffer(jpg_buffer, dtype='uint8'), -1)
+    input_batch = torch.tensor(image).cuda()
+    boxes, labels, probs = predictor_m2.predict(input_batch, 150, 150, 30, 0.4)
+    imageHub.send_reply(b'OK')
+    times.append(time.time())
+    
+    if len(times) > 1:
+        print(1./(times[-1] - times[-2]))
 
-    while True:
-        # hash_type = connbuf.get_utf8()
-        # if not hash_type:
-        #     break
-        # print('hash type: ', hash_type)
 
-        file_name = connbuf.get_utf8()
-        if not file_name:
-            break
-        file_name = os.path.join("uploads", file_name)
-        print("file name: ", file_name)
 
-        file_size = int(connbuf.get_utf8())
-        print("file size: ", file_size)
+########################################################################################
 
-        with open(file_name, "wb") as f:
-            remaining = file_size
-            while remaining:
-                chunk_size = 4096 if remaining >= 4096 else remaining
-                chunk = connbuf.get_bytes(chunk_size)
-                if not chunk:
-                    break
-                f.write(chunk)
-                remaining -= len(chunk)
-            # if remaining:
-            #     print('File incomplete.  Missing',remaining,'bytes.')
-            # else:
-            #     print('File received successfully.')
-        with open(file_name) as json_file:
-            input_batch, height, width = json.load(json_file).values()
-        input_batch = torch.tensor(input_batch).cuda()
-        boxes, labels, probs = predictor_m2.predict(input_batch, height, width, 30, 0.4)
-        times.append(time.time())
-    print("Connection closed.")
-    conn.close()
-    break
+# s = socket.socket()
+# s.bind((HOST, PORT))
+# s.listen(10)
+# print("Waiting for a connection.....")
 
-output = [times[i] - times[i - 1] for i in range(1, len(times))]
-print(output)
+# while True:
+#     conn, addr = s.accept()
+#     print("Got a connection from ", addr)
+#     connbuf = buffer.Buffer(conn)
+
+#     while True:
+#         # hash_type = connbuf.get_utf8()
+#         # if not hash_type:
+#         #     break
+#         # print('hash type: ', hash_type)
+
+#         file_name = connbuf.get_utf8()
+#         if not file_name:
+#             break
+#         file_name = os.path.join("uploads", file_name)
+#         print("file name: ", file_name)
+
+#         file_size = int(connbuf.get_utf8())
+#         print("file size: ", file_size)
+
+#         with open(file_name, "wb") as f:
+#             remaining = file_size
+#             while remaining:
+#                 chunk_size = 4096 if remaining >= 4096 else remaining
+#                 chunk = connbuf.get_bytes(chunk_size)
+#                 if not chunk:
+#                     break
+#                 f.write(chunk)
+#                 remaining -= len(chunk)
+#             # if remaining:
+#             #     print('File incomplete.  Missing',remaining,'bytes.')
+#             # else:
+#             #     print('File received successfully.')
+#         with open(file_name) as json_file:
+#             input_batch, height, width = json.load(json_file).values()
+#             input_batch = torch.tensor(input_batch).cuda()
+#             boxes, labels, probs = predictor_m2.predict(input_batch, height, width, 30, 0.4)
+#             times.append(time.time())
+#     print("Connection closed.")
+#     conn.close()
+#     break
+
+# output = [times[i] - times[i - 1] for i in range(1, len(times))]
+# print(output)
--- sender.py
+++ sender.py
@@ -8,61 +8,93 @@ import json
 import socket
 import threading
 import os
+import imagezmq
+import argparse
+import cv2
+import time 
 
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-s", "--server-ip", required=True,
+        help="ip address of the server to which the client will connect")
+    ap.add_argument("-v", "--video-file", required=True,
+        help="path to a video file", type=str)
+    ap.add_argument("-q", "--jpeg-quality", required=False,
+        help="0 to 100, higher is better quality, 95 is cv2 default", type=int, default=95)
+    ap.add_argument("-m", "--model-path", required=False,
+        help="path to the trained model", type=str, default="./models/ufpark-model.pth")
+    ap.add_argument("-l", "--label-path", required=False,
+        help="SSD labels", type=str, default="./models/ufpark-model-labels.txt")
+    ap.add_argument("-p", "--split-point", required=False,
+        help="split points on SSD: 0, 1, 2, 3 or 4", type=int, default=0)
+    args = vars(ap.parse_args())
 
-import buffer
+    class_names = [name.strip() for name in open(args["label_path"]).readlines()]
 
-print("# Preparing the client model.")
+    print("# Creating ssd model.")
+    model = create_vgg_ssd(len(class_names), is_test=True)
 
-model_path = "./models/ufpark-model.pth"
-split = 0  # Split point int the first maxpooling layer.
-label_path = "./models/ufpark-model-labels.txt"
+    model.load(args["model_path"])
+    model1, _ = create_vgg_ssd_new(len(class_names), model, split=args["split_point"], is_test=True)
 
-class_names = [name.strip() for name in open(label_path).readlines()]
+    predictor_m1 = PredictorM1(model1, config.image_size, config.image_mean, device=None,)
+    print("# Done!")
 
-print("# Creating ssd model.")
-model = create_vgg_ssd(len(class_names), is_test=True)
+    sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(args["server_ip"]))
+    rpiName = socket.gethostname()
+    cap = cv2.VideoCapture(args["video_file"])
+    time.sleep(2.0)
 
-model.load(model_path)
-model1, _ = create_vgg_ssd_new(len(class_names), model, split=split, is_test=True)
+    while True:
+        # read the frame from the camera and send it to the server
+        # frame = vs.read()
+        ret, frame = cap.read()
+        if ret == True:
+            output = predictor_m1.predict(frame)
+            output = output.cpu().numpy()
 
-predictor_m1 = PredictorM1(model1, config.image_size, config.image_mean, device=None,)
-print("# Done!")
+            # ret_code, jpg_buffer = cv2.imencode(".jpg", output, [int(cv2.IMWRITE_JPEG_QUALITY), args["jpeg_quality"]])
+            sender.send_image(rpiName, jpg_buffer[0])
+        else:
+            break
 
-HOST = "200.239.93.134"
-PORT = 2345
+# # import the necessary packages
+# from imutils.video import VideoStream
+# import imagezmq
+# import argparse
+# import socket
+# import time
+# import cv2
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
+# # construct the argument parser and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-s", "--server-ip", required=True,
+# 	help="ip address of the server to which the client will connect")
+# ap.add_argument("-v", "--video-file", required=True,
+# 	help="path to a video file", type=str)
+# ap.add_argument("-q", "--jpeg-quality", required=False,
+# 	help="0 to 100, higher is better quality, 95 is cv2 default", type=int, default=95)
+# args = vars(ap.parse_args())
+# # initialize the ImageSender object with the socket address of the
+# # server
+# sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
+# 	args["server_ip"]))
 
-with s:
-    sbuf = buffer.Buffer(s)
-
-    # hash_type = input('Enter hash type: ')
-
-    # files = input('Enter file(s) to send: ')
-    # files_to_send = files.split()
-
-    for file_name in os.listdir("images"):
-        # print(file_name)
-        # sbuf.put_utf8(hash_type)
-        sbuf.put_utf8(file_name.split("/")[-1])
-
-        orig_image = cv2.imread("./images/" + file_name)
-        image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
-        height, width, _ = image.shape
-        output = predictor_m1.predict(image)
-
-        output = output.cpu().numpy().tolist()
-
-        filename = "data.json"
-        with open(filename, "w") as outfile:
-            json.dump({"scores": output, "height": height, "width": width}, outfile)
-
-        file_size = os.path.getsize(filename)
-        sbuf.put_utf8(str(file_size))
-
-        with open(filename, "rb") as f:
-            sbuf.put_bytes(f.read())
-        print("File Sent")
-        os.remove(filename)
+# # get the host name, initialize the video stream, and allow the
+# # camera sensor to warmup
+# rpiName = socket.gethostname()
+# # vs = VideoStream(usePiCamera=False).start()
+# #vs = VideoStream(src=0).start()
+# print(args["video_file"])
+# cap = cv2.VideoCapture(args["video_file"])
+# time.sleep(2.0)
+ 
+# while True:
+# 	# read the frame from the camera and send it to the server
+# 	# frame = vs.read()
+# 	ret, frame = cap.read()
+# 	if ret == True:
+# 		ret_code, jpg_buffer = cv2.imencode(".jpg", frame, [int(cv2.IMWRITE_JPEG_QUALITY), args["jpeg_quality"]])
+# 		sender.send_image(rpiName, jpg_buffer)
+# 	else:
+# 		break
